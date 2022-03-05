128743a13207254dc1b815573886311a2a521db9
rimji <88181976+rimji@users.noreply.github.com>
2022-03-05 16:15:16 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -38,7 +38,7 @@
              &nbsp&nbsp
              <a href="https://www.babybaby.in/contact/">Contact us</a>
              <br>
-             &copy; BabyBaby
+             &copy; BabyBaby.in
         </center></small>
         </footer>
     </section>
